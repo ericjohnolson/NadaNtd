4d1bd270d9832e7e09266ee46c53c1dd397f6bf5
ericjohnolson <ericjolson@gmail.com>
2015-04-12 15:49:04 -0700

    audited elimination report for #39

--- Nada.Tests/ExcelWorksheetGeneration.cs
+++ Nada.Tests/ExcelWorksheetGeneration.cs
@@ -18,153 +18,156 @@ namespace Nada.Tests
     [TestClass]
     public class ExcelWorksheetGeneration : BaseTest
     {
-        [TestMethod]
-        public void CanCreateTranslationKeys()
-        {
-            DataTable table = new DataTable();
-            Dictionary<string, DataRow> rowDict = new Dictionary<string, DataRow>();
-            table.Columns.Add(new DataColumn("Key"));
-            table.Columns.Add(new DataColumn("English"));
-            table.Columns.Add(new DataColumn("French"));
-            table.Columns.Add(new DataColumn("Portuguese"));
-            table.Columns.Add(new DataColumn("Bahasa"));
+        ///
+        /// Moved to Nada.Utilities.DeveloperUtility
+        ///
+        //[TestMethod]
+        //public void CanCreateTranslationKeys()
+        //{
+        //    DataTable table = new DataTable();
+        //    Dictionary<string, DataRow> rowDict = new Dictionary<string, DataRow>();
+        //    table.Columns.Add(new DataColumn("Key"));
+        //    table.Columns.Add(new DataColumn("English"));
+        //    table.Columns.Add(new DataColumn("French"));
+        //    table.Columns.Add(new DataColumn("Portuguese"));
+        //    table.Columns.Add(new DataColumn("Bahasa"));
 
-            // id-ID;Bahasa|en-US;English|fr-FR;Français|pt-PT;Português"
+        //    // id-ID;Bahasa|en-US;English|fr-FR;Français|pt-PT;Português"
 
-            var english = new TranslationLookupInstance(new CultureInfo("en-US"));
-            foreach(var key in english.Keys)
-            {
-                var dr = table.NewRow();
-                dr["Key"] = key;
-                dr["English"] = english.GetValue(key);
-                table.Rows.Add(dr);
-                rowDict.Add(key, dr);
-            }
+        //    var english = new TranslationLookupInstance(new CultureInfo("en-US"));
+        //    foreach(var key in english.Keys)
+        //    {
+        //        var dr = table.NewRow();
+        //        dr["Key"] = key;
+        //        dr["English"] = english.GetValue(key);
+        //        table.Rows.Add(dr);
+        //        rowDict.Add(key, dr);
+        //    }
 
-            var french = new TranslationLookupInstance(new CultureInfo("fr-FR"));
-            foreach (var key in english.Keys)
-            {
-                var dr = rowDict[key];
-                dr["French"] = french.GetValue(key);
-            }
-            var port = new TranslationLookupInstance(new CultureInfo("pt-PT"));
-            foreach (var key in english.Keys)
-            {
-                var dr = rowDict[key];
-                dr["Portuguese"] = port.GetValue(key);
-            }
+        //    var french = new TranslationLookupInstance(new CultureInfo("fr-FR"));
+        //    foreach (var key in english.Keys)
+        //    {
+        //        var dr = rowDict[key];
+        //        dr["French"] = french.GetValue(key);
+        //    }
+        //    var port = new TranslationLookupInstance(new CultureInfo("pt-PT"));
+        //    foreach (var key in english.Keys)
+        //    {
+        //        var dr = rowDict[key];
+        //        dr["Portuguese"] = port.GetValue(key);
+        //    }
            
-            var bahasa = new TranslationLookupInstance(new CultureInfo("id-ID"));
-            foreach (var key in english.Keys)
-            {
-                var dr = rowDict[key];
-                dr["Bahasa"] = bahasa.GetValue(key);
-            }
+        //    var bahasa = new TranslationLookupInstance(new CultureInfo("id-ID"));
+        //    foreach (var key in english.Keys)
+        //    {
+        //        var dr = rowDict[key];
+        //        dr["Bahasa"] = bahasa.GetValue(key);
+        //    }
             
 
-            using (ExcelPackage pck = new ExcelPackage())
-            {
-                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Sheet1");
-                ws.Cells["A1"].LoadFromDataTable(table, true);
-                File.WriteAllBytes(string.Format(@"C:\Users\Eric\Desktop\Iota Ink\TranslationKeys_{0}.xlsx", DateTime.Now.ToString("yyyyMMdd")), pck.GetAsByteArray());
-            }
-        }
+        //    using (ExcelPackage pck = new ExcelPackage())
+        //    {
+        //        ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Sheet1");
+        //        ws.Cells["A1"].LoadFromDataTable(table, true);
+        //        File.WriteAllBytes(string.Format(@"C:\Users\Eric\Desktop\Iota Ink\TranslationKeys_{0}.xlsx", DateTime.Now.ToString("yyyyMMdd")), pck.GetAsByteArray());
+        //    }
+        //}
 
-        [TestMethod]
-        public void CanCreateIndicatorUpdateForm()
-        {
-            DataTable table = new DataTable();
-            table.Columns.Add(new DataColumn("Indicator Id"));
-            //table.Columns.Add(new DataColumn("IsDisabled"));
-            table.Columns.Add(new DataColumn("Type Id"));
-            table.Columns.Add(new DataColumn("Type Name"));
-            table.Columns.Add(new DataColumn("Form Name"));
-            table.Columns.Add(new DataColumn("Indicator Name"));
-            table.Columns.Add(new DataColumn("Indicator Type"));
-            table.Columns.Add(new DataColumn("Is Required"));
-            table.Columns.Add(new DataColumn("Aggregation Rule"));
-            table.Columns.Add(new DataColumn("Aggregation Rule ID"));
-            table.Columns.Add(new DataColumn("Merge Rule"));
-            table.Columns.Add(new DataColumn("Merge Rule ID"));
-            table.Columns.Add(new DataColumn("Split Rule"));
-            table.Columns.Add(new DataColumn("Split Rule ID"));
-            ReportRepository repo = new ReportRepository();
+        //[TestMethod]
+        //public void CanCreateIndicatorUpdateForm()
+        //{
+        //    DataTable table = new DataTable();
+        //    table.Columns.Add(new DataColumn("Indicator Id"));
+        //    //table.Columns.Add(new DataColumn("IsDisabled"));
+        //    table.Columns.Add(new DataColumn("Type Id"));
+        //    table.Columns.Add(new DataColumn("Type Name"));
+        //    table.Columns.Add(new DataColumn("Form Name"));
+        //    table.Columns.Add(new DataColumn("Indicator Name"));
+        //    table.Columns.Add(new DataColumn("Indicator Type"));
+        //    table.Columns.Add(new DataColumn("Is Required"));
+        //    table.Columns.Add(new DataColumn("Aggregation Rule"));
+        //    table.Columns.Add(new DataColumn("Aggregation Rule ID"));
+        //    table.Columns.Add(new DataColumn("Merge Rule"));
+        //    table.Columns.Add(new DataColumn("Merge Rule ID"));
+        //    table.Columns.Add(new DataColumn("Split Rule"));
+        //    table.Columns.Add(new DataColumn("Split Rule ID"));
+        //    ReportRepository repo = new ReportRepository();
 
-            List<ReportIndicator> indicators = new List<ReportIndicator>();
-            indicators = repo.GetDiseaseDistroIndicators();
-            foreach (var cmpc in indicators)
-                foreach (var cat in cmpc.Children)
-                    AddInds(table, cat.Children, cat.Name, IndicatorEntityType.DiseaseDistribution);
-            indicators = repo.GetSurveyIndicators();
-            foreach (var cmpc in indicators)
-                foreach (var cat in cmpc.Children)
-                    AddInds(table, cat.Children, cat.Name, IndicatorEntityType.Survey);
-            indicators = repo.GetIntvIndicators();
-            foreach (var cmpc in indicators)
-                foreach (var cat in cmpc.Children)
-                    AddInds(table, cat.Children, cat.Name, IndicatorEntityType.Intervention);
-            indicators = repo.GetProcessIndicators();
-            foreach (var cmpc in indicators)
-                foreach (var cat in cmpc.Children)
-                    AddInds(table, cat.Children, cat.Name, IndicatorEntityType.Process);
+        //    List<ReportIndicator> indicators = new List<ReportIndicator>();
+        //    indicators = repo.GetDiseaseDistroIndicators();
+        //    foreach (var cmpc in indicators)
+        //        foreach (var cat in cmpc.Children)
+        //            AddInds(table, cat.Children, cat.Name, IndicatorEntityType.DiseaseDistribution);
+        //    indicators = repo.GetSurveyIndicators();
+        //    foreach (var cmpc in indicators)
+        //        foreach (var cat in cmpc.Children)
+        //            AddInds(table, cat.Children, cat.Name, IndicatorEntityType.Survey);
+        //    indicators = repo.GetIntvIndicators();
+        //    foreach (var cmpc in indicators)
+        //        foreach (var cat in cmpc.Children)
+        //            AddInds(table, cat.Children, cat.Name, IndicatorEntityType.Intervention);
+        //    indicators = repo.GetProcessIndicators();
+        //    foreach (var cmpc in indicators)
+        //        foreach (var cat in cmpc.Children)
+        //            AddInds(table, cat.Children, cat.Name, IndicatorEntityType.Process);
 
-            ProcessRepository prepo = new ProcessRepository();
-            ProcessBase saes = prepo.Create(9);
-            foreach (var i in saes.ProcessType.Indicators)
-                indicators.Add(ReportRepository.CreateReportIndicator(saes.ProcessType.Id, i));
-            AddInds(table, indicators, saes.ProcessType.TypeName, IndicatorEntityType.Process);
+        //    ProcessRepository prepo = new ProcessRepository();
+        //    ProcessBase saes = prepo.Create(9);
+        //    foreach (var i in saes.ProcessType.Indicators)
+        //        indicators.Add(ReportRepository.CreateReportIndicator(saes.ProcessType.Id, i));
+        //    AddInds(table, indicators, saes.ProcessType.TypeName, IndicatorEntityType.Process);
 
-            using (ExcelPackage pck = new ExcelPackage())
-            {
-                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Sheet1");
-                ws.Cells["A1"].LoadFromDataTable(table, true);
-                File.WriteAllBytes(string.Format(@"C:\Users\Eric\Desktop\Iota Ink\AllIndicatorRules_{0}.xlsx", DateTime.Now.ToString("yyyyMMdd")), pck.GetAsByteArray());
-            }
-        }
+        //    using (ExcelPackage pck = new ExcelPackage())
+        //    {
+        //        ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Sheet1");
+        //        ws.Cells["A1"].LoadFromDataTable(table, true);
+        //        File.WriteAllBytes(string.Format(@"C:\Users\Eric\Desktop\Iota Ink\AllIndicatorRules_{0}.xlsx", DateTime.Now.ToString("yyyyMMdd")), pck.GetAsByteArray());
+        //    }
+        //}
 
-        [TestMethod]
-        public void CanCreateIndicatorOptionUpdateForm()
-        {
-            DataTable table = new DataTable();
-            table.Columns.Add(new DataColumn("Indicator Id"));
-            table.Columns.Add(new DataColumn("Type Id"));
-            table.Columns.Add(new DataColumn("Type Name"));
-            table.Columns.Add(new DataColumn("Form Name"));
-            table.Columns.Add(new DataColumn("Indicator Name"));
-            table.Columns.Add(new DataColumn("Indicator Option ID"));
-            table.Columns.Add(new DataColumn("Indicator Option"));
-            table.Columns.Add(new DataColumn("Weighted Ranking"));
-            ReportRepository repo = new ReportRepository();
-            List<ReportIndicator> indicators = repo.GetDiseaseDistroIndicators();
-            foreach (var cmpc in indicators)
-                foreach (var cat in cmpc.Children)
-                    AddOptions(table, cat.Children.Where(x => x.DataTypeId == (int)IndicatorDataType.Dropdown), cat.Name, IndicatorEntityType.DiseaseDistribution);
-            indicators = repo.GetSurveyIndicators();
-            foreach (var cmpc in indicators)
-                foreach (var cat in cmpc.Children)
-                    AddOptions(table, cat.Children.Where(x => x.DataTypeId == (int)IndicatorDataType.Dropdown), cat.Name, IndicatorEntityType.Survey);
-            indicators = repo.GetIntvIndicators();
-            foreach (var cmpc in indicators)
-                foreach (var cat in cmpc.Children)
-                    AddOptions(table, cat.Children.Where(x => x.DataTypeId == (int)IndicatorDataType.Dropdown), cat.Name, IndicatorEntityType.Intervention);
-            indicators = repo.GetProcessIndicators();
-            foreach (var cmpc in indicators)
-                foreach (var cat in cmpc.Children)
-                    AddOptions(table, cat.Children.Where(x => x.DataTypeId == (int)IndicatorDataType.Dropdown), cat.Name, IndicatorEntityType.Process);
+        //[TestMethod]
+        //public void CanCreateIndicatorOptionUpdateForm()
+        //{
+        //    DataTable table = new DataTable();
+        //    table.Columns.Add(new DataColumn("Indicator Id"));
+        //    table.Columns.Add(new DataColumn("Type Id"));
+        //    table.Columns.Add(new DataColumn("Type Name"));
+        //    table.Columns.Add(new DataColumn("Form Name"));
+        //    table.Columns.Add(new DataColumn("Indicator Name"));
+        //    table.Columns.Add(new DataColumn("Indicator Option ID"));
+        //    table.Columns.Add(new DataColumn("Indicator Option"));
+        //    table.Columns.Add(new DataColumn("Weighted Ranking"));
+        //    ReportRepository repo = new ReportRepository();
+        //    List<ReportIndicator> indicators = repo.GetDiseaseDistroIndicators();
+        //    foreach (var cmpc in indicators)
+        //        foreach (var cat in cmpc.Children)
+        //            AddOptions(table, cat.Children.Where(x => x.DataTypeId == (int)IndicatorDataType.Dropdown), cat.Name, IndicatorEntityType.DiseaseDistribution);
+        //    indicators = repo.GetSurveyIndicators();
+        //    foreach (var cmpc in indicators)
+        //        foreach (var cat in cmpc.Children)
+        //            AddOptions(table, cat.Children.Where(x => x.DataTypeId == (int)IndicatorDataType.Dropdown), cat.Name, IndicatorEntityType.Survey);
+        //    indicators = repo.GetIntvIndicators();
+        //    foreach (var cmpc in indicators)
+        //        foreach (var cat in cmpc.Children)
+        //            AddOptions(table, cat.Children.Where(x => x.DataTypeId == (int)IndicatorDataType.Dropdown), cat.Name, IndicatorEntityType.Intervention);
+        //    indicators = repo.GetProcessIndicators();
+        //    foreach (var cmpc in indicators)
+        //        foreach (var cat in cmpc.Children)
+        //            AddOptions(table, cat.Children.Where(x => x.DataTypeId == (int)IndicatorDataType.Dropdown), cat.Name, IndicatorEntityType.Process);
 
-            ProcessRepository prepo = new ProcessRepository();
-            ProcessBase saes = prepo.Create(9);
-            foreach (var i in saes.ProcessType.Indicators)
-                indicators.Add(ReportRepository.CreateReportIndicator(saes.ProcessType.Id, i));
-            AddOptions(table, indicators.Where(x => x.DataTypeId == (int)IndicatorDataType.Dropdown), saes.ProcessType.TypeName, IndicatorEntityType.Process);
+        //    ProcessRepository prepo = new ProcessRepository();
+        //    ProcessBase saes = prepo.Create(9);
+        //    foreach (var i in saes.ProcessType.Indicators)
+        //        indicators.Add(ReportRepository.CreateReportIndicator(saes.ProcessType.Id, i));
+        //    AddOptions(table, indicators.Where(x => x.DataTypeId == (int)IndicatorDataType.Dropdown), saes.ProcessType.TypeName, IndicatorEntityType.Process);
 
-            using (ExcelPackage pck = new ExcelPackage())
-            {
-                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Sheet1");
-                ws.Cells["A1"].LoadFromDataTable(table, true);
-                File.WriteAllBytes(string.Format(@"C:\Users\Eric\Desktop\Iota Ink\IndicatorDropdownWeightedRules_{0}.xlsx", DateTime.Now.ToString("yyyyMMdd")), pck.GetAsByteArray());
-            }
-        }
+        //    using (ExcelPackage pck = new ExcelPackage())
+        //    {
+        //        ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Sheet1");
+        //        ws.Cells["A1"].LoadFromDataTable(table, true);
+        //        File.WriteAllBytes(string.Format(@"C:\Users\Eric\Desktop\Iota Ink\IndicatorDropdownWeightedRules_{0}.xlsx", DateTime.Now.ToString("yyyyMMdd")), pck.GetAsByteArray());
+        //    }
+        //}
 
         #region Extra utilities
         //[TestMethod]
