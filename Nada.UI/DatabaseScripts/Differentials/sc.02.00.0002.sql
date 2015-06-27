88858de07aba896c962f14b17313b6f2c15ea9e8
bretten <brettinbox@gmail.com>
2015-06-26 23:50:44 -0700

    199 - Changed critical cutoff decision indicator options

--- Nada.UI/DatabaseScripts/Differentials/sc.02.00.0002.sql
+++ Nada.UI/DatabaseScripts/Differentials/sc.02.00.0002.sql
@@ -1,6 +1,12 @@
 -- Add indicator num rounds of pc... story # 198;
 insert into SurveyIndicators (DataTypeId, DisplayName, AggTypeId, SortOrder, UpdatedById, UpdatedAt, IsDisabled, IsEditable, IsRequired, IsDisplayed, IsCalculated, CanAddValues, SurveyTypeId, RedistrictRuleId, MergeRuleId) values (2, '4190984d-f272-4359-8414-6e7ef06fc4bc', 5, 250, 26, NOW(), 0, 0, 0, 0, 0, 0, 15, 2, 54);
 
+-- Story #199 Critical decision dropdowns
+DELETE FROM IndicatorDropdownValues WHERE ID IN (616, 617);
+INSERT INTO IndicatorDropdownValues (IndicatorId, EntityType, SortOrder, DropdownValue, TranslationKey, WeightedValue, UpdatedById, UpdatedAt, CreatedById, CreatedAt) VALUES (566, 3, 1, 'TASCutoffStopMDA', 'TASCutoffStopMDA', 0, 26, NOW, 26, NOW());
+INSERT INTO IndicatorDropdownValues (IndicatorId, EntityType, SortOrder, DropdownValue, TranslationKey, WeightedValue, UpdatedById, UpdatedAt, CreatedById, CreatedAt) VALUES (566, 3, 2, 'TASCutoffContinueMDA', 'TASCutoffContinueMDA', 0, 26, NOW, 26, NOW());
+INSERT INTO IndicatorDropdownValues (IndicatorId, EntityType, SortOrder, DropdownValue, TranslationKey, WeightedValue, UpdatedById, UpdatedAt, CreatedById, CreatedAt) VALUES (566, 3, 3, 'TASCutoffContinueSurveillance', 'TASCutoffContinueSurveillance', 0, 26, NOW, 26, NOW());
+
 
 INSERT INTO [SchemaChangeLog]
        ([MajorReleaseNumber]
