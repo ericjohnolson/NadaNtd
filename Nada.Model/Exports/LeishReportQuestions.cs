58fa5d9a4f31e01689999c6bcaf2ff5c3b71cc05
bretten <brettinbox@gmail.com>
2015-08-09 16:02:43 -0700

    220 - Added leish export ui validation

--- Nada.Model/Exports/LeishReportQuestions.cs
+++ Nada.Model/Exports/LeishReportQuestions.cs
@@ -1,4 +1,5 @@
-﻿using Nada.Model.Base;
+﻿using Nada.Globalization;
+using Nada.Model.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,5 +25,77 @@ namespace Nada.Model.Exports
         public string LeishRepRelapseDefCl { get; set; }
         public string LeishRepFailureDefVl { get; set; }
         public string LeishRepFailureDefCl { get; set; }
+
+        #region IDataErrorInfo Members
+        public override string this[string columnName]
+        {
+            get
+            {
+                string error = "";
+                switch (columnName)
+                {
+                    case "LeishRepYearReporting":
+                        if (!LeishRepYearReporting.HasValue)
+                            error = Translations.Required;
+                        else if (LeishRepYearReporting.Value > 2100 || LeishRepYearReporting.Value < 1900)
+                            error = Translations.ValidYear;
+                        break;
+                    case "LeishRepEndemicAdmin2Vl":
+                        if (!LeishRepEndemicAdmin2Vl.HasValue)
+                            error = Translations.Required;
+                        break;
+                    case "LeishRepEndemicAdmin2Cl":
+                        if (!LeishRepEndemicAdmin2Cl.HasValue)
+                            error = Translations.Required;
+                        break;
+                    case "LeishRepYearLncpEstablished":
+                        if (!LeishRepYearLncpEstablished.HasValue)
+                            error = Translations.Required;
+                        else if (LeishRepYearLncpEstablished.Value > 2100 || LeishRepYearLncpEstablished.Value < 1900)
+                            error = Translations.ValidYear;
+                        break;
+                    case "LeishRepUrlLncp":
+                        if (string.IsNullOrEmpty(LeishRepUrlLncp))
+                            error = Translations.Required;
+                        break;
+                    case "LeishRepYearLatestGuide":
+                        if (!LeishRepYearLatestGuide.HasValue)
+                            error = Translations.Required;
+                        else if (LeishRepYearLatestGuide.Value > 2100 || LeishRepYearLatestGuide.Value < 1900)
+                            error = Translations.ValidYear;
+                        break;
+                    case "LeishRepNumHealthFac":
+                        if (!LeishRepNumHealthFac.HasValue)
+                            error = Translations.Required;
+                        break;
+                    case "LeishRepAntiMedInNml":
+                        if (string.IsNullOrEmpty(LeishRepAntiMedInNml))
+                            error = Translations.Required;
+                        break;
+                    case "LeishRepRelapseDefVl":
+                        if (string.IsNullOrEmpty(LeishRepRelapseDefVl))
+                            error = Translations.Required;
+                        break;
+
+                    case "LeishRepRelapseDefCl":
+                        if (string.IsNullOrEmpty(LeishRepRelapseDefCl))
+                            error = Translations.Required;
+                        break;
+                    case "LeishRepFailureDefVl":
+                        if (string.IsNullOrEmpty(LeishRepFailureDefVl))
+                            error = Translations.Required;
+                        break;
+                    case "LeishRepFailureDefCl":
+                        if (string.IsNullOrEmpty(LeishRepFailureDefCl))
+                            error = Translations.Required;
+                        break;
+                    default: error = "";
+                        break;
+                }
+                return error;
+            }
+        }
+
+        #endregion
     }
 }
