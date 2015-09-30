00bd57831687aabed3afce6534020348c21a159e
bretten <brettinbox@gmail.com>
2015-09-30 12:00:31 -0700

    Updated survey import validation to account for multi admin levels per survey

--- Nada.Model/Imports/SurveyImporter.cs
+++ Nada.Model/Imports/SurveyImporter.cs
@@ -254,36 +254,25 @@ namespace Nada.Model.Imports
             bool valid = true;
 
             List<SurveyBase> objs = new List<SurveyBase>();
-            foreach (DataRow row in ds.Tables[0].Rows)
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
             {
+                DataRow row = ds.Tables[0].Rows[i];
+                // Uniquely identify the row
+                string rowIdentifier;
+                if (ds.Tables[0].Columns.Contains("* " + TranslationLookup.GetValue("ID")) && row["* " + TranslationLookup.GetValue("ID")].ToString().Length > 0)
+                    rowIdentifier = row["* " + TranslationLookup.GetValue("ID")].ToString();
+                else if (ds.Tables[0].Columns.Contains("* " + TranslationLookup.GetValue("SurveyName")) && row["* " + TranslationLookup.GetValue("SurveyName")].ToString().Length > 0)
+                    rowIdentifier = row["* " + TranslationLookup.GetValue("SurveyName")].ToString();
+                else
+                    rowIdentifier = i.ToString();
                 // Build an object and get its indicators
-                if (row["* " + TranslationLookup.GetValue("ID")] == null || row["* " + TranslationLookup.GetValue("ID")].ToString().Length == 0)
-                    continue;
                 string objerrors = "";
                 SurveyBase obj = repo.CreateSurvey(sType.Id);
-                int adminLevelId = Convert.ToInt32(row["* " + TranslationLookup.GetValue("ID")]);
-                obj.AdminLevels = new List<AdminLevel> { new AdminLevel { Id = adminLevelId } };
                 obj.Notes = row[TranslationLookup.GetValue("Notes")].ToString();
                 obj.IndicatorValues = GetDynamicIndicatorValues(ds, row, ref objerrors);
 
-                // Need to get the meta data
+                // No meta data for surveys
                 List<KeyValuePair<string, string>> metaData = new List<KeyValuePair<string, string>>();
-                // First get the DateReported indicator value
-                IndicatorValue indicatorValueToCompareAgainst = obj.IndicatorValues.FirstOrDefault(v => v.Indicator.DisplayName == "DateReported");
-                // Use the DateReported value to determine the meta data as described in Mingle story 169
-                if (indicatorValueToCompareAgainst != null && obj.AdminLevelId.HasValue)
-                {
-                    DateTime dateReported;
-                    if (DateTime.TryParse(indicatorValueToCompareAgainst.DynamicValue, out dateReported))
-                    {
-                        // Determine the start and end date for the same year as the DateReported
-                        DateTime start = new DateTime(dateReported.Year, 1, 1, 0, 0, 0, dateReported.Kind);
-                        DateTime end = start.AddYears(1);
-                        // Get the meta data
-                        metaData = Calculator.GetMetaData(Indicators.Where(i => !i.Value.IsCalculated && i.Value.DataTypeId == (int)IndicatorDataType.Calculated).Select(i => new KeyValuePair<string, string>(sType.DisplayNameKey, i.Value.DisplayName)).ToList(),
-                            obj.AdminLevelId.Value, start, end);
-                    }
-                }
 
                 // Validate the object
                 List<ValidationResult> validationResults = Validator.ValidateIndicators(sType.DisplayNameKey, translatedIndicators, obj.IndicatorValues, metaData);
@@ -292,7 +281,7 @@ namespace Nada.Model.Imports
                 foreach (ValidationResult validationResult in validationResults)
                 {
                     // Add the validation results to the string
-                    validationResultStr += string.Format("ID {0}: {1}{2}{3}{4}", row["* " + TranslationLookup.GetValue("ID")].ToString(), validationResult.Message, Environment.NewLine, "--------", Environment.NewLine);
+                    validationResultStr += string.Format("ID {0}: {1}{2}{3}{4}", rowIdentifier, validationResult.Message, Environment.NewLine, "--------", Environment.NewLine);
                     // See if this set of data has already been marked as invalid
                     if (valid && !validationResult.IsSuccess)
                         valid = false;
