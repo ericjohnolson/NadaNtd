08e69931409dd4c1092afd93674c6591b4fa10ca
bretten <brettinbox@gmail.com>
2016-02-19 11:48:29 -0800

    Added database update file for release 02.00.0005

---
+++ Nada.UI/DatabaseScripts/Differentials/sc.02.00.0005.sql
@@ -0,0 +1,16 @@
+﻿--Story 5;
+UPDATE Surveys SET SiteTypeId = 1 WHERE SiteType IN ('Sentinel', 'Sentinelle', 'sentinela');
+UPDATE Surveys SET SiteTypeId = 2 WHERE SiteType IN ('Spot Check', 'Vérification ponctuelle', 'Spot check', 'verificar no local');
+
+INSERT INTO [SchemaChangeLog]
+       ([MajorReleaseNumber]
+       ,[MinorReleaseNumber]
+       ,[PointReleaseNumber]
+       ,[ScriptName]
+       ,[DateApplied])
+VALUES
+       ('02'
+       ,'00'
+       ,'0005'
+       ,'sc.02.00.0005.sql'
+       ,Now());
